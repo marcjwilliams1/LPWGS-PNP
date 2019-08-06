b3255d1465924a34e4da97a794d10638953c0ec0
Marc Williams <hfx042@frontend11.apocrita>
2019-08-06 19:14:00 +0100

    Changes to report

--- rules/report.smk
+++ rules/report.smk
@@ -6,14 +6,17 @@ rule report:
     output:
         report="reports/results.html",
         plotdir=directory(config["workdirectory"] + "reports/plots/")
+    params:
+        readscutoff=config["readscutoff"]
     shell:
         """
         #mkdir {output.plotdir}
         module load singularity
         singularity exec /data/BCI-EvoCa2/marc/singularity/marcjwilliams1-r-tidy-markdown-bio-master-latest.simg \
         Rscript /data/BCI-EvoCa2/marc/anisha/LPWGS-PNP/scripts/report.R \
-        --QC {input.QC} \
-        --output {output.report} \
-        --CNA {input.CNA} \
-        --plotdir {output.plotdir}
+            --QC {input.QC} \
+            --output {output.report} \
+            --CNA {input.CNA} \
+            --plotdir {output.plotdir} \
+            --readscutoff {params.readscutoff}
         """
--- scripts/report.R
+++ scripts/report.R
@@ -11,11 +11,15 @@ parser$add_argument('--CNA', type = 'character',
                     help="CNA file")
 parser$add_argument('--plotdir', type = 'character',
                     help="Plotting directory")
+parser$add_argument('--readscutoff', type = 'character',
+                    help="Cut off to exclude reads from samples")
 args <- parser$parse_args()
+print(args)
 
 dfQC <- read_csv(args$QC)
 CNA <- readRDS(args$CNA)
 print(dfQC)
+print(typeof(args$readscutoff))
 
 render("/data/BCI-EvoCa2/marc/anisha/LPWGS-PNP/scripts/report.Rmd",
     "html_document",
@@ -23,4 +27,4 @@ render("/data/BCI-EvoCa2/marc/anisha/LPWGS-PNP/scripts/report.Rmd",
     output_file = basename(args$output),
     intermediates_dir = dirname(args$output),
     clean = FALSE,
-    params = list(dfQC = dfQC, CNA = CNA, plotdir = args$plotdir))
+    params = list(dfQC = dfQC, CNA = CNA, plotdir = args$plotdir, readscutoff = args$readscutoff))
